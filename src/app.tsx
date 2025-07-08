81334e5adc84d1a40e5f346f3f8ea693d3e40e4c
MarcosAntonio15243 <marcosantonio15243@gmail.com>
2025-07-08 19:14:18 -0300

    chore: add Tanstack React Query configuration

--- src/app.tsx
+++ src/app.tsx
@@ -1,14 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Rooms } from "./pages/rooms";
 import { RoomDetails } from "./pages/room-details";
+import { Rooms } from "./pages/rooms";
 
 export function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={ <Rooms /> } index />
-        <Route element={ <RoomDetails /> } path="/room" />
+        <Route element={<Rooms />} index />
+        <Route element={<RoomDetails />} path="/room" />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
