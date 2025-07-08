801db2a07b01e848ebff289975f527b18acd81c8
MarcosAntonio15243 <marcosantonio15243@gmail.com>
2025-07-08 19:15:54 -0300

    chore: format codebase using biome

--- src/pages/rooms.tsx
+++ src/pages/rooms.tsx
@@ -1,13 +1,11 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 export const Rooms = () => {
   return (
     <div>
-
       <h1>Rooms</h1>
 
       <Link to="/room">Go to details</Link>
-
     </div>
-  )
-}
+  );
+};
